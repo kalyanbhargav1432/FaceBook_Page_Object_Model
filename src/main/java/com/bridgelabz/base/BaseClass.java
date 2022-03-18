80829ce7dcc30f6474b80a8f90fae04985d71a64
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2022-03-18 11:59:47 +0530

    Added:To capture the screeshot while test case was failed

--- src/main/java/com/bridgelabz/base/BaseClass.java
+++ src/main/java/com/bridgelabz/base/BaseClass.java
@@ -4,18 +4,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.bridgelabz.utility.UtilityClass;
+import com.bridgelabz.utility.WebEventListener;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public static WebDriver driver;
 	public static Properties properties;
+	public static EventFiringWebDriver eventFiringWebdriver;
+	public static WebEventListener eventListener;
 
 	public BaseClass() {
 		properties = new Properties();
@@ -38,13 +42,19 @@ public class BaseClass {
 		} else if (browserName.equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		} 
-			 driver.manage().window().maximize();
-		        driver.manage().deleteAllCookies();
-		        driver.manage().timeouts().pageLoadTimeout(UtilityClass.PAGE_LOAD_TIME, TimeUnit.SECONDS);
-		        driver.manage().timeouts().implicitlyWait(UtilityClass.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
-		        driver.get(properties.getProperty("url"));
-		
+		}
+		// Capture screenshot when test failed
+		eventFiringWebdriver = new EventFiringWebDriver(driver);
+		eventListener = new WebEventListener();
+		eventFiringWebdriver.register(eventListener);
+		driver = eventFiringWebdriver;
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(UtilityClass.PAGE_LOAD_TIME, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(UtilityClass.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+		driver.get(properties.getProperty("url"));
+
 	}
 
 }
--- src/main/java/com/bridgelabz/utility/UtilityClass.java
+++ src/main/java/com/bridgelabz/utility/UtilityClass.java
@@ -1,19 +1,22 @@
 package com.bridgelabz.utility;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import com.bridgelabz.base.BaseClass;
 
 public class UtilityClass extends BaseClass {
-	public static long PAGE_LOAD_TIME = 45;
-	public static long IMPLICIT_WAIT_TIME = 40;
+	public static long PAGE_LOAD_TIME = 15;
+	public static long IMPLICIT_WAIT_TIME = 10;
 	public static String TESTDATA_SHEET_PATH = "C:\\Users\\ADMIN\\eclipse-workspace\\Java-Programs\\FaceBookPageObjectModel\\src\\main\\resources\\FacedBookLoginPage.xlsx";
 
 	public static Workbook book;
@@ -46,4 +49,15 @@ public class UtilityClass extends BaseClass {
 		}
 		return data;
 	}
+
+	public static void takeScreenshotAtEndOfTest() {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("webdriver.chrome.driver\",\"C:\\Users\\ADMIN\\Downloads\\chromedriver_win32//chromedriver.exe");
+        try {
+            FileUtils.copyFile(scrFile, new File("./screenshots/" + System.currentTimeMillis() / 1000 + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
